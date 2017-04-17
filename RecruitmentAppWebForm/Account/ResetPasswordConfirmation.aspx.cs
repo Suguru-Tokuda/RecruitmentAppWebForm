00fe393e1781770079f19db28fca0c5cbe08d457
IT485S702 <IT485S702@WEBDEV10-15>
2017-04-16 19:46:14 -0500

    Add project files.

---
+++ RecruitmentAppWebForm/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace RecruitmentAppWebForm.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ RecruitmentAppWebForm/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecruitmentAppWebForm.Startup))]
+namespace RecruitmentAppWebForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
