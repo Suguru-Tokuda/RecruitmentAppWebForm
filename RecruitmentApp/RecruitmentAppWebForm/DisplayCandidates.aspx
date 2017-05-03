f89abc4d70809484aafc9b8e5e71f78aaaafab17
IT485S702 <IT485S702@WEBDEV10-15>
2017-05-03 01:53:02 -0500

    updated

--- RecruitmentApp/RecruitmentAppWebForm/DisplayCandidates.aspx
+++ RecruitmentApp/RecruitmentAppWebForm/DisplayCandidates.aspx
@@ -4,18 +4,38 @@
 
 
     <label>Job title:</label>
-    <asp:Label runat="server" ID="jobTitle" />
-
+    <asp:Label runat="server" ID="jobTitle" Text="" />
     <div class="row">
-        <div class="col-md-6 col-sm-6 col-xs-6">
-            <asp:ListView ID="applicantsListView" runat="server">
+
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <asp:DataList DataKeyField="applicant_id" OnSelectedIndexChanged="applicantList_SelectedIndexChanged" CssClass="table-bordered" ID="applicantList" runat="server">
+                <HeaderTemplate>
+                    <label class="text-align: center">Applicants</label>
+                </HeaderTemplate>
+                <HeaderStyle BackColor="#00ffcc" />
                 <ItemTemplate>
-                    <asp:Label runat="server" ID="candidateFirstName" />
-                    <asp:Label runat="server" ID="candidateLastName" />
+                    <asp:LinkButton Text='<%# Eval("first_name") + " " + Eval("last_name") %>' CommandName="Select" runat="server" />
+                    <asp:Label ID="applicantID" runat="server" Visible="false" Text='<%# Eval("applicant_id") %>' />
                 </ItemTemplate>
-            </asp:ListView>
+            </asp:DataList>
         </div>
+
+        <div class="col-md-4">
+            <asp:DataList runat="server" ID="candidateDetails">
+                <ItemTemplate>
+                    <label>Name</label>
+                    <asp:Label runat="server" Text='<%# Eval("first_name") + " " + Eval("last_name") %>' />
+                </ItemTemplate>
+            </asp:DataList>
+
+
+
+        </div>
+
+
+
     </div>
+
 
 
 
--- RecruitmentApp/RecruitmentAppWebForm/DisplayCandidates.aspx.cs
+++ RecruitmentApp/RecruitmentAppWebForm/DisplayCandidates.aspx.cs
@@ -10,24 +10,54 @@ namespace RecruitmentAppWebForm
 {
     public partial class DisplayCandidates : System.Web.UI.Page
     {
+        Applicant applicant;
+        int applicantID;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            int job_id = (int)HttpContext.Current.Session["job_id"];
+            applicant = (Applicant)HttpContext.Current.Session["applicant"];
+            if (applicant == null)
+            {
+                Session["loggedIn"] = true;
+            }
+            else
+            {
+                if (applicant.loggedIn != false)
+                {
+                    Session["loggedIn"] = true;
+                }
+            }
 
-            jobTitle.Text = JobsDB.getJobTitle(job_id);
+            //int job_id = (int)HttpContext.Current.Session["job_id"];
 
-            applicantsListView.DataSource = ApplicantDB.getApplicants(job_id);
-            applicantsListView.DataBind();
+            jobTitle.Text = JobsDB.getJobTitle(1);
+
+            applicantList.DataSource = ApplicantDB.getApplicants(1);
+            applicantList.DataBind();
 
         }
 
+        protected void applicantID_Click(object sender, EventArgs e)
+        {
+            DataListCommandEventArgs f = (DataListCommandEventArgs)e;
+            int index = f.Item.ItemIndex;
 
+            //int index = (int)applicantList.SelectedValue;
+            
+            //Control id = applicantList.Items[applicantList.SelectedIndex].FindControl("applicantID");
+            //Session["applicant_id"] = Convert.ToInt32(applicantList.Items[applicantList.SelectedIndex].FindControl("applicantID"));
+        }
 
+        private void showApplicantDetails()
+        {
+            List<Applicant> list = ApplicantDB.getApplicant(applicantID);
+            candidateDetails.DataSource = list;
+            candidateDetails.DataBind();
+        }
 
-
-
-
-
-
+        protected void applicantList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            applicantID = Convert.ToInt32(((System.Web.UI.WebControls.Label)applicantList.Items[applicantList.SelectedIndex].FindControl("applicantID")).Text);
+        }
     }
 }
