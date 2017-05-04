0b6bdfb9741d03b7701d0b6e3b0a0e6599cda813
IT485S701 <IT485S701@WEBDEV10-14>
2017-05-04 00:18:06 -0500

    Committing changes

--- RecruitmentApp/RecruitmentAppWebForm/Apply.aspx
+++ RecruitmentApp/RecruitmentAppWebForm/Apply.aspx
@@ -1,4 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Apply.aspx.cs" Inherits="RecruitmentAppWebForm.Apply" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="col-md-8">
+        <div class="table table-bordered table-condensed">
 
+        </div>
+    </div>
 </asp:Content>
--- RecruitmentApp/RecruitmentAppWebForm/Models/JobsDB.cs
+++ RecruitmentApp/RecruitmentAppWebForm/Models/JobsDB.cs
@@ -13,6 +13,47 @@ namespace RecruitmentAppWebForm.Models
     [DataObject(true)]
     public class JobsDB
     {
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public static List<Job> getApplicantJobs(int applicantId)
+        {
+            List<Job> retVal = new List<Job>();
+            string sql = "SELECT DISTINCT* FROM jobs AS j JOIN applications AS a ON j.job_id = a.job_id where a.applicant_id  = @applicantId";
+            using (SqlConnection con = new SqlConnection(getConnectionString()))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    con.Open();
+                    cmd.Parameters.AddWithValue("applicantId", applicantId);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    Job job;
+                    while (dr.Read())
+                    {
+                        job = new Models.Job();
+                        job.job_id = Convert.ToInt32(dr["job_id"].ToString());
+                        job.company_id = Convert.ToInt32(dr["company_id"].ToString());
+                        job.company_name = dr["company_name"].ToString();
+                        job.category = dr["category"].ToString();
+                        job.position = dr["position"].ToString();
+                        job.salary_min = Convert.ToInt32(dr["salary_min"].ToString());
+                        job.salary_max = Convert.ToInt32(dr["salary_max"].ToString());
+                        job.level = dr["level"].ToString();
+                        job.description = dr["description"].ToString();
+                        job.qualification = dr["qualification"].ToString();
+                        job.posting_date = Convert.ToDateTime(dr["posting_date"].ToString());
+                        job.filled = Convert.ToBoolean(dr["filled"].ToString());
+                        job.jobCity = dr["city"].ToString();
+                        job.jobState = dr["state"].ToString();
+                        retVal.Add(job);
+                    }
+
+                    dr.Close();
+                }
+
+            }
+
+            return retVal;
+            
+        }
         [DataObjectMethod (DataObjectMethodType.Select )]
         public static List<Job> displayJobs(int jobId)
         {
